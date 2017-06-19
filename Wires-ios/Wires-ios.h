31cad82fe8c3a1765c5de64fd836e0cb5f78270d
Elviro Rocca <elviro.rocca@facile.it>
2017-06-19 12:24:54 +0200

    added Wires-ios target

---
+++ Wires-ios/Wires-ios.h
@@ -0,0 +1,19 @@
+//
+//  Wires-ios.h
+//  Wires-ios
+//
+//  Created by Elviro Rocca on 19/06/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Wires-ios.
+FOUNDATION_EXPORT double Wires_iosVersionNumber;
+
+//! Project version string for Wires-ios.
+FOUNDATION_EXPORT const unsigned char Wires_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wires_ios/PublicHeader.h>
+
+
