6a4701e61ef48c6aa0463c3736a79717898b0826
Elviro Rocca <elviro.rocca@icloud.com>
2017-07-02 14:52:18 +0200

    added Future type

--- Sources/ProducerTypes.swift
+++ Sources/ProducerTypes.swift
@@ -60,3 +60,62 @@ extension Fixed where T: Sequence {
 		return self
 	}
 }
+
+public final class Future<T>: Producer {
+	public typealias ProducedType = T
+
+	private let execute: ((T) -> ()) -> ()
+	private let speaker: Speaker<T>
+	private var fixed: Fixed<T>? = nil
+
+	private var currentState: State = .idle {
+		didSet {
+			guard case .complete(let value) = currentState else { return }
+			speaker.say(value)
+			fixed = Fixed.init(value, productionQueue: speaker.productionQueue)
+		}
+	}
+
+	public var productionQueue: DispatchQueue {
+		if let fixed = fixed {
+			return fixed.productionQueue
+		} else {
+			return speaker.productionQueue
+		}
+	}
+
+	public init(execute: @escaping ((T) -> ()) -> (), on productionQueue: DispatchQueue = .main) {
+		self.execute = execute
+		self.speaker = Speaker.init(productionQueue: productionQueue)
+	}
+
+	public var value: T? {
+		guard case .complete(let value) = currentState else { return nil }
+		return value
+	}
+
+	public func start() -> Self {
+		guard case .idle = currentState else { return self }
+		currentState = .processing
+		execute { [weak self] value in
+			guard let this = self else { return }
+			this.currentState = .complete(value)
+		}
+		return self
+	}
+
+	public func upon(_ callback: @escaping (Signal<T>) -> ()) -> Self {
+		if let fixed = fixed {
+			fixed.upon(callback)
+		} else {
+			speaker.upon(callback)
+		}
+		return self
+	}
+
+	private enum State {
+		case idle
+		case processing
+		case complete(T)
+	}
+}
