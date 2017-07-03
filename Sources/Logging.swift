645b26ff7a5ef2057126e8ff87895f0b73fec3c9
Elviro Rocca <elviro.rocca@facile.it>
2017-07-03 15:06:18 +0200

    improved logging

--- Sources/Logging.swift
+++ Sources/Logging.swift
@@ -1,14 +1,17 @@
 public enum Preferences {
-	private static let logActiveKey = "Wires.Preferences.logActiveKey"
 	public static var logActive: Bool = false
+	public static var logSeparator: String? = nil
 }
 
 enum Log {
 	static let prefix = "WiresLog"
-	static let separator = " --> "
+	static let separator = "------"
+	static let interfix = " --> "
 
 	static func with(context: Any, text: String) {
 		guard Preferences.logActive else { return }
-		print(prefix + separator + "\(context)" + separator + text)
+		let preseparator = Preferences.logSeparator.map { $0 + "\n" } ?? ""
+		let postseparator = Preferences.logSeparator.map { "\n" + $0 } ?? ""
+		print(preseparator + prefix + interfix + "\(context)" + interfix + text + postseparator)
 	}
 }
